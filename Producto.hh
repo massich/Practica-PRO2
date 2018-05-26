06e35f85ad8c08db27f3241188c0f168f8548a67
martivall <martivalloliveira@gmail.com>
2018-05-26 16:38:53 +0200

    especificació clases+main

---
+++ Producto.hh
@@ -0,0 +1,80 @@
+/** @file Producto.hh
+    @brief Especificación de la clase Producto
+*/
+
+#ifndef _PRODUCTO_HH_
+#define _PRODUCTO_HH_
+
+#ifndef NO_DIAGRAM
+#include <string>
+#include "almacen.hh"
+#endif
+
+
+/** @class Producto
+    @brief Representa un tipo de producto del almacen. 
+
+*/
+
+class Producto {
+ 
+ public:
+  //Constructora
+  
+  /** @brief añadir producto al sistema
+      \pre identificador no existe
+      \post El resultado es añadir un producto con 0 unidades e identificador "id" a la lista de productos del almacen
+  */  
+  void poner_prod(string id);
+  
+  //Modificadoras
+
+  /** @brief Modificadora de los atributos
+      \pre unidades>=0 y el identifcador empieza con una letra
+      \post El parámetro implícito pasa a tener identificador "id" y "ud" unidades.
+      */
+  void modificar(string id, int ud);
+  
+  //Consultoras
+
+  /** @brief Consultora del identificador
+      \pre <em>cierto</em>
+      \post El resultado es el identificador del parámetro implícito
+  */
+  int consul_identificador() const;
+ 
+  /** @brief Consultora de las unidades
+      \pre <em>cierto</em>
+      \post El resultado es el numero de unidades del parámetro implícito
+  */
+  bool consul_unidades() const;
+  
+  //Destructora
+  
+  /** @brief Dar de baja un producto
+      \pre identificador empieza por letra y unidades == 0
+      \post El resultado es dar de baja el producto con identificador "id" de la lista de productos
+  */  
+  void quitar_prod(string id);
+    
+  //Escritura de producto
+
+  /** @brief Operación de escritura
+
+      \pre <em>cierto</em>
+      \post Se han escrito el identificador y el numero de unidades del producto
+  */
+  void escribir_producto() const;
+
+private:
+  string identificador;
+  int unidades;
+};
+
+#endif
+  
+  
+  
+  
+  
+  
---
+++ main.cc
@@ -0,0 +1,59 @@
+  /**
+   * @mainpage Función main de la pràctica TreeKEA.
+
+En este proyecto se representa un almacén, con sus respectivas salas. Se introducen las clases "Almacen", "Sala" y "Producto". Además se usa alguna eterna como el BInTree.hh.
+Los productos se meten en una ista de productos dentro de la clase almacen para poder hacer el inventario y poder acceder a cada uno de los productos en todo momento. Además la clase almacen llama siempre a la clase sala para las funciones, de modo que la estructura del programa se centra en el almacen y deriva los trabajos a otras clases dependiendo de lo que se tenga que hacer.
+
+Sólo se documentan elementos públicos
+*/
+
+/** @file main.cc
+    @brief Programa principal para el ejercicio "TreeKEA".
+*/
+
+
+
+#include "producto.hh"
+#include "sala.hh"
+#include "almacen.hh"
+
+#ifndef NO_DIAGRAM
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+int main() {
+    string a;
+    bool fin = false
+    
+    While(fin == false & cin >> a) {
+        
+    if(a == "poner_prod") {}
+    
+    else if(a == "quitar_prod") {}
+    
+    else if(a == "poner_items") {}
+     
+    else if(a == "quitar_items") {}
+      
+    else if(a == "distribuir") {}
+       
+    else if(a == "compactar") {}
+     
+    else if(a == "reorganizar") {}
+      
+    else if(a == "redimensionar") {}
+     
+    else if(a == "inventario") {}
+      
+    else if(a == "escribir") {}
+     
+    else if(a == "consultar_pos") {}
+     
+    else if(a == "consultar_prod") {}
+      
+    else if(a == "fin")fin  == true;
+    
+    }
+}
